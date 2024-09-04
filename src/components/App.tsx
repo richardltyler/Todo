41f5efa4be0956e8c2be512ecdacf03c0640ab4a
Richard Tyler <richardtyler2011@gmail.com>
2024-09-04 11:04:20 -0500

    Add data hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
-import { useContext, useEffect, useRef, useState } from "react";
 import TaskList from "./TaskList.tsx";
 import NewTask from "./NewTask.tsx";
-import { ActionTypes, sortTasks, useAppContext } from "../context.tsx";
 
 export type Todo = {
   id: number;
@@ -16,18 +14,6 @@ export type Data = {
 };
 
 function App() {
-  const { dispatch } = useAppContext();
-  useEffect(() => {
-    dispatch({
-      type: ActionTypes.AddAll,
-      payload: sortTasks([
-        { id: 0, task: "Gym", isComplete: true },
-        { id: 1, task: "Tan", isComplete: true },
-        { id: 2, task: "Laundry", isComplete: false },
-      ]),
-    });
-  });
-
   return (
     <div className='App' data-testid='App'>
       <NewTask />
